13e5041e1b0bc52584158439075e7f1a1a962e1c
Young <634046906@qq.com>
2020-10-29 18:01:23 +0800

    js数据结构-链表类80%

---
+++ js-data-structure/LinkedList/inex.js
@@ -0,0 +1,95 @@
+/**
+ * @description 单项链表类
+ * @author Young
+ */
+class Node {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.length = 0
+    }
+    append (el) {
+        let node = new Node(el)
+        // 若长度为0 直接让头指针指向该节点
+        if (this.length === 0) {
+            this.head = node
+        } else {
+            // 链式查找到最后一个
+            let current = this.head
+            while (current.next) {
+                current = current.next
+            }
+            current.next = node
+        }
+        this.length++
+        return this
+    }
+    insert (el, position) { }
+    get (position) {
+        if (position < 0 || position > this.length - 1) return 'invalid parameter'
+        let current = this.head
+        let count = 0
+        while (position > count++) {
+            current = current.next
+        }
+        return current
+    }
+    set (el, position) {
+        let node = this.get(position)
+        node.data = el
+        return true
+    }
+    indexOf (el) {
+        if (this.head === null) return -1
+        let current = this.head
+        let index = 0
+        while (current) {
+            if (current.data === el) {
+                return index
+            }
+            current = current.next
+            index++
+        }
+        return -1
+
+    }
+    remove (el) {
+        let index = this.indexOf(el)
+        return this.removeAt(index)
+    }
+    removeAt (position) {
+        if (position < 0 || position > this.length - 1) return 'invalid parameter'
+        this.length--
+        if (position === 0) {
+            this.head = this.head.next
+            return true
+        } else {
+            let prev = this.get(position - 1)
+            prev.next = prev.next.next
+            return true
+        }
+
+    }
+    toString () {
+        let str = 'head=>'
+        let current = this.head
+        while (current) {
+            str = str + current.data + '=>'
+            current = current.next
+        }
+        return str
+    }
+
+}
+let t = new LinkedList()
+t.append(1).append(2).append(3).append(4).append(5)
+// t.set('333', 1)
+t.removeAt(0)
+console.log(t.toString())
+// export default LinkedList
